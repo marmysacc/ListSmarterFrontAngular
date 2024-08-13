6bb8de9d49892630162ffb563f82914de37c579e
mmysiarski <mmysiarski@objectivity.co.uk>
2024-08-13 10:18:01 +0200

    Styling add bucket form

--- src/app/buckets/components/add-bucket/add-bucket.component.ts
+++ src/app/buckets/components/add-bucket/add-bucket.component.ts
@@ -1,6 +1,6 @@
-import { Component, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-bucket',
@@ -20,24 +20,9 @@ export class AddBucketComponent {
       color: ['#964b00', Validators.required],
       maxNumberOfTasks: ['15', Validators.required],
     });
-    // this.profileForm.valueChanges.subscribe(value => {
-    //   console.log('valueChanges',value.name);
-    // })
   }
-
-  // onSubmit() {
-  //   // TODO: Use EventEmitter with form value
-  //   //console.warn(this.profileForm.value);
-  //   //this.bucket=this.profileForm.value;
-  //   // this.addBucket(this.bucket);
-  //   console.log("addBucket", this.profileForm)
-  // }
 
   closeDialog() {
     this.dialogRef.close(false);
   }
-
-  // addBucket(bucket: AddBucketModel){
-  //   this.bucketservice.addBucket(bucket).subscribe();
-  // }
 }
