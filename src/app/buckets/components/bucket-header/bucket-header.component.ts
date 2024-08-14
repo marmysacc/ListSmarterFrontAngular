4a3b7d37ec9c1a5930e43a30a60e69b160c76736
mmysiarski <mmysiarski@objectivity.co.uk>
2024-08-14 09:45:02 +0200

    Fix delete button and refactor the code

--- src/app/buckets/components/bucket-header/bucket-header.component.ts
+++ src/app/buckets/components/bucket-header/bucket-header.component.ts
@@ -1,11 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { BucketService } from 'src/app/core/services/bucket.service';
-import { DialogService } from 'src/app/core/services/dialog.service';
-import { BucketinhomeModel } from '../../models/bucket-in-home-model';
-import { EditBucketComponent } from '../edit-bucket/edit-bucket.component';
+import { BucketModel } from '../../models/bucket-model';
 
 @Component({
   selector: 'app-bucket-header',
@@ -13,26 +7,6 @@ import { EditBucketComponent } from '../edit-bucket/edit-bucket.component';
   styleUrls: ['./bucket-header.component.scss']
 })
 export class BucketHeaderComponent {
-  @Input() bucket: Observable<BucketinhomeModel> | undefined;
-  constructor(private dialogservice: DialogService,
-    private dialog: MatDialog) { }
-
-  // onEditBucket() {    
-  //   const dialogOpen = this.dialog.open(EditBucketComponent,
-  //     {        
-  //         width: '660px'
-  //     });
-  // }
-
-
-
-
-  // ngOnInit() {
-  //   this.router.params.subscribe((res: any) => {
-  //     console.log(res.id);
-  //     const bucketId: number = +res.id;
-  //     this.bucket = this.bucketservice.getBucketById(bucketId);   
-  //     console.log(this.bucket);
-  //  });
+  @Input() bucketModel: BucketModel | undefined;
 }
 
--- src/app/single-bucket-page/single-bucket-page.component.ts
+++ src/app/single-bucket-page/single-bucket-page.component.ts
@@ -9,6 +9,8 @@ import { DialogService } from '../core/services/dialog.service';
 import { EditBucketComponent } from '../buckets/components/edit-bucket/edit-bucket.component';
 import { MatDialog } from '@angular/material/dialog';
 import { EditBucketModel } from '../buckets/models/edit-bucket-model';
+import { BucketModel } from '../models/bucket-model';
+import { statisticModel } from '../models/statistic-model';
 
 @Component({
   selector: 'app-single-bucket-page',
@@ -17,35 +19,34 @@ import { EditBucketModel } from '../buckets/models/edit-bucket-model';
 })
 export class SinglebucketComponent implements OnInit {
   welcome = 'Welcome in ListSmarter';
+  bucketModel: BucketModel | undefined;
+  statisticModel: statisticModel | undefined;
   statistics: Observable<TaskStateModel[]> | undefined;
   bucket: Observable<BucketinhomeModel> | undefined;
   onDeleteBucket: EventEmitter<BucketinhomeModel> = new EventEmitter();
   id: Number = {} as Number;
-  backgroundColor: string;
   constructor(
     private statisticsservice: StatisticsService,
     private bucketservice: BucketService,
     private router: ActivatedRoute,
     private routing: Router,
-    private dialogservice: DialogService,
+    private dialogService: DialogService,
     private dialog: MatDialog
   ) {
     const navigation = this.routing.getCurrentNavigation();
-    this.backgroundColor = navigation?.extras.state?.['backgroundColor'] || 'defaultColor';
   }
 
   ngOnInit(): void {
-    this.readBucket();
-    console.log("color", this.backgroundColor)
+    this.getBucket();
   }
 
-  readBucket(){
+  getBucket(){
     this.router.params.subscribe((res: any) => {
-      console.log(res.id);
       const bucketId: number = +res.id;
+      this.bucketservice.getBucketById(bucketId).subscribe((bucket => {
+        this.bucketModel = bucket;
+      }));
       this.statistics = this.statisticsservice.getStatisticByBucket(bucketId);
-      this.bucket = this.bucketservice.getBucketById(bucketId);
-      console.log(this.bucket);
       this.id=bucketId;
     });
   }
@@ -56,28 +57,22 @@ export class SinglebucketComponent implements OnInit {
       data: this.id,
     });
     dialogOpen.afterClosed().subscribe((result: EditBucketModel) => {
-      console.log('afterClosed', result);
       if(result){
-        console.log("dsd", result);
-        this.bucketservice.editBucket(result).subscribe(()=>this.readBucket());
+        this.bucketservice.editBucket(result).subscribe(()=>this.getBucket());
       }
     })
   }
 
-  // onDelete() {
-  //   this.dialogservice
-  //     .openConfirmDialog(`Are you sure to delete bucket ${this.bucket} with all tasks?`)
-  //     .afterClosed()
-  //     .subscribe((res) => {
-  //       if (res) {
-  //         this.onDeleteBucket.emit(this.bucket);
-  //       }
-  //     });
-  // }
-
-  // deleteBucket(bucket: BucketinhomeModel) {
-  //   this.bucketservice
-  //     .deleteBucket(bucket)
-  //     .subscribe();
-  // }
+  onDelete() {
+    this.dialogService
+      .openConfirmDialog(`Are you sure to delete bucket ${this.bucketModel?.name} with all tasks?`)
+      .afterClosed()
+      .subscribe((res) => {
+        if (res && this.bucketModel) {
+          this.bucketservice.deleteBucket(this.bucketModel).subscribe(() => {
+            this.routing.navigate(['/']);
+          });
+        }
+      });
+  }
 }
