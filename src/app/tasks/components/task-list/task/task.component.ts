741f20796d984c49c8f4e329307a2bee38d5c4cd
mmysiarski <mmysiarski@objectivity.co.uk>
2024-08-20 14:49:35 +0200

    Add static popup for edit tasks

--- src/app/tasks/components/task-list/task/task.component.ts
+++ src/app/tasks/components/task-list/task/task.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { TaskModel } from 'src/app/tasks/models/task-model';
 import { TaskService } from '../../../../core/services/task.service';
+import { EditTaskComponent } from '../../edit-task/edit-task.component';
+import { EditTaskModel } from 'src/app/tasks/models/edit-task-model';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-task',
@@ -9,7 +12,7 @@ import { TaskService } from '../../../../core/services/task.service';
 })
 export class TaskComponent implements OnInit {
   @Input() task: TaskModel = {} as TaskModel;
-  constructor(private taskService: TaskService) {}
+  constructor(private taskService: TaskService, private dialog: MatDialog) {}
 
   ngOnInit() {}
 
@@ -18,5 +21,19 @@ export class TaskComponent implements OnInit {
     this.taskService
       .editTask(this.task)
       .subscribe((task) => (this.task = task));
+  }
+
+  onEditTask() {
+    const dialogOpen = this.dialog.open(EditTaskComponent, {
+      width: '660px',
+      data: this.task,
+    });
+    dialogOpen.afterClosed().subscribe((result: EditTaskModel) => {
+      if (result) {
+        this.taskService
+          .editTask(result)
+          .subscribe(() => this.taskService.getTaskById(this.task.id));
+      }
+    });
   }
 }
